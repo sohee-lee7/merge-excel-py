e5bcbddbf566fae36e38c048cc23d160350b1118
sohee-lee7 <sohee6030@gmail.com>
2022-06-03 00:44:02 +0900

    enhance: merge connect gui

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from merge import merge_excel
 
 # 윈도우 창 만들기
 window = tk.Tk()
@@ -10,30 +11,47 @@ window.geometry("640x400+100+100")
 window.resizable(False, False)
 
 
+directory = ''
+target_header = ''
+regex = ''
+
 # 폴더 선택 함수
-def selectFolder():
+def select_folder():
   # 폴더 선택하는 창 띄우기
-  dirName = filedialog.askdirectory(parent=window)
+  directory = filedialog.askdirectory(parent=window)
+
   # 선택된 폴더 이름 라벨에 보여주기
-  folderLabel.configure(text="선택된 폴더: " + dirName)
-  print(dirName)
-  # 선택된 폴더가 있을 때만 머지버튼 활성화
-  if dirName:
-    mergeBtn['state'] = 'normal'
-  else:
-    mergeBtn['state'] = 'disabled'
+  folder_label.configure(text="선택된 폴더: " + directory)
 
 
 # 폴더 선택 버튼 생성 및 눌렀을 때 selectFolder 호출
-folderBtn = tk.Button(window, text="폴더 선택", command=selectFolder)
-folderBtn.pack()
+folder_btn = tk.Button(window, text="폴더 선택", command=select_folder)
+folder_btn.pack()
 
 # 선택된 폴더 보여주기 위한 텍스트라벨
-folderLabel = tk.Label(window, text="선택된 폴더")
-folderLabel.pack()
+folder_label = tk.Label(window, text="선택된 폴더")
+folder_label.pack()
+
+# 변경할 컬럼 헤더 이름
+header_entry = tk.Entry(window)
+header_entry.pack()
+
+# 정규식
+regex_entry = tk.Entry(window)
+regex_entry.insert(0, '-[a-zA-Z0-9]*')
+regex_entry.pack()
+
+def merge_btn_command():
+  print('merge_btn_command')
+  if directory & header_entry.get() & regex_entry.get():
+    merge_btn['state'] = 'normal'
+    merge_excel(directory=directory, target_header=header_entry.get(), regex=regex_entry.get())
+  else:
+    merge_btn['state'] = 'disabled'
+
 
 # 머지를 실행할 버튼
-mergeBtn = tk.Button(window, text="Merge", command=selectFolder, state='disabled')
-mergeBtn.pack()
+merge_btn = tk.Button(window, text="Merge", command=merge_btn_command, state='disabled')
+merge_btn.pack()
 
 window.mainloop()
--- merge.py
+++ merge.py
@@ -1,37 +1,39 @@
 import os
-import re
 import pyexcel as px
 from format import format_data_array
 
-directory = '/Users/soheelee/Desktop/python-data'
-filelist = os.listdir(directory)
+# directory: 폴더 위치
+# target_header: 포맷팅할 헤더 이름
+# regex: 포맷할 정규식
+def merge_excel(directory, target_header, regex):
+  filelist = os.listdir(directory)
 
-result = {}
+  result = {}
 
-for filename in filelist:
-  if '.xlsx' not in filename:
-    continue
-  file_dir = directory + '/' + filename
+  for filename in filelist:
+    if '.xlsx' not in filename:
+      continue
+    file_dir = directory + '/' + filename
 
-  # file 읽기
-  book = px.get_book(file_name=file_dir)
+    # file 읽기
+    book = px.get_book(file_name=file_dir)
 
-  # file 각 시트 데이터 가져오기
-  for sheetname in book.sheet_names():
-    sheet = px.get_sheet(file_name=file_dir, sheet_name=sheetname)
-    origin_data = sheet.get_array()
+    # file 각 시트 데이터 가져오기
+    for sheetname in book.sheet_names():
+      sheet = px.get_sheet(file_name=file_dir, sheet_name=sheetname)
+      origin_data = sheet.get_array()
 
-    format_data = format_data_array(origin_data, 'HEAD_C', '-[a-zA-Z0-9]*')
+      format_data = format_data_array(origin_data, target_header, regex)
 
-    # 시트별로 데이터를 분류하기 위해 directory 형태로 저장
-    if sheetname in result:
-      # 이미 directory에 있는 sheet면 헤더 제외하고 data 합치기
-      result[sheetname] += format_data[1:]
-    else:
-      # directory에 없는 sheet면 헤더가 있는 전체 format_data 넣기
-      result[sheetname] = format_data
+      # 시트별로 데이터를 분류하기 위해 directory 형태로 저장
+      if sheetname in result:
+        # 이미 directory에 있는 sheet면 헤더 제외하고 data 합치기
+        result[sheetname] += format_data[1:]
+      else:
+        # directory에 없는 sheet면 헤더가 있는 전체 format_data 넣기
+        result[sheetname] = format_data
 
 
-# 합산된 시트 데이터를 하나의 파일로 만들기
-for key in result.keys():
-  px.save_as(array=result[key], dest_file_name=directory + '/' + key + '.csv', sheet_name=key)
+  # 합산된 시트 데이터를 하나의 파일로 만들기
+  for key in result.keys():
+    px.save_as(array=result[key], dest_file_name=directory + '/' + key + '.csv', sheet_name=key)
